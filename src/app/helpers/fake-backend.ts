bb9add23211d5f30b463a1e6e18544875d506279
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-06-23 16:03:34 +0300

    Add CRUD Routes to Fake Backend API

--- src/app/helpers/fake-backend.ts
+++ src/app/helpers/fake-backend.ts
@@ -20,6 +20,14 @@ export class FakeBackendInterceptor implements HttpInterceptor {
                     return authenticate();
                 case url.endsWith('/users/register') && method === 'POST':
                     return register();
+                case url.endsWith('/users') && method === 'GET':
+                    return getUsers();
+                case url.match(/\/users\/\d+$/) && method === 'GET':
+                    return getUserById();
+                case url.match(/\/users\/\d+$/) && method === 'PUT':
+                    return updateUser();
+                case url.match(/\/users\/\d+$/) && method === 'DELETE':
+                    return deleteUser();
                 default:
                     // pass through any requests not handled above
                     return next.handle(request);
@@ -45,6 +53,45 @@ export class FakeBackendInterceptor implements HttpInterceptor {
             localStorage.setItem(usersKey, JSON.stringify(users));
             return ok();
         }
+
+        function getUsers() {
+            if (!isLoggedIn()) return unauthorized();
+            return ok(users.map(x => basicDetails(x)));
+        }
+
+        function getUserById() {
+            if (!isLoggedIn()) return unauthorized();
+
+            const user = users.find(x => x.id === idFromUrl());
+            return ok(basicDetails(user));
+        }
+
+        function updateUser() {
+            if (!isLoggedIn()) return unauthorized();
+
+            let params = body;
+            let user = users.find(x => x.id === idFromUrl());
+
+            // only update password if entered
+            if (!params.password) {
+                delete params.password;
+            }
+
+            // update and save user
+            Object.assign(user, params);
+            localStorage.setItem(usersKey, JSON.stringify(users));
+
+            return ok();
+        }
+
+        function deleteUser() {
+            if (!isLoggedIn()) return unauthorized();
+
+            users = users.filter(x => x.id !== idFromUrl());
+            localStorage.setItem(usersKey, JSON.stringify(users));
+            return ok();
+        }
+
         // helper functions
         function ok(body?: any) {
             return of(new HttpResponse({ status: 200, body }))
@@ -54,9 +101,24 @@ export class FakeBackendInterceptor implements HttpInterceptor {
             return throwError(() => ({ error: { message } }))
                 .pipe(materialize(), delay(500), dematerialize()); // call materialize and dematerialize to ensure delay even if an error is thrown (https://github.com/Reactive-Extensions/RxJS/issues/648);
         }
+
+        function unauthorized() {
+            return throwError(() => ({ status: 401, error: { message: 'Unauthorized' } }))
+                .pipe(materialize(), delay(500), dematerialize());
+        }
+
         function basicDetails(user: any) {
             const { id, username, firstName, lastName } = user;
             return { id, username, firstName, lastName };
+        }
+
+        function isLoggedIn() {
+            return headers.get('Authorization') === 'Bearer fake-jwt-token';
+        }
+
+        function idFromUrl() {
+            const urlParts = url.split('/');
+            return parseInt(urlParts[urlParts.length - 1]);
         }
     }
 }
