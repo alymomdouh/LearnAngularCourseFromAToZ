af0ed93559a46411c1733a9cb17bab74e2e43527
alyxmomdouh@gmail.com <alyxmomdouh@gmail.com>
2023-06-23 15:01:23 +0300

    add ErrorInterceptor ,JwtInterceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,8 +7,8 @@ import { LoginComponent } from './Auth/components/login/login.component';
 import { RegisterComponent } from './Auth/components/register/register.component';
 import { HomeComponent } from './Auth/components/home/home.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { fakeBackendProvider } from './helpers';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { ErrorInterceptor, JwtInterceptor, fakeBackendProvider } from './helpers';
 
 @NgModule({
   declarations: [
@@ -24,6 +24,8 @@ import { fakeBackendProvider } from './helpers';
     AppRoutingModule
   ],
   providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     // provider used to create fake backend
     fakeBackendProvider
   ],
